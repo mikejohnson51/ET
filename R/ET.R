92c948244a598d70590febd916081c7eb8e50658
mikejohnson51 <jmj00@umail.ucsb.edu>
2018-05-04 10:06:23 -0700

    readxl add

---
+++ R/ET.R
@@ -0,0 +1,25 @@
+#' ET
+#'
+#' \code{ET} package
+#'
+#' MODELING ET AT VARIABLE TEMPORAL AND SPATIAL SCALES
+#'
+#' See the README on
+#'
+#' @docType package
+#' @name ET
+#'
+#'@importFrom graphics axis par plot text
+#'@importFrom grDevices dev.off png
+#'@importFrom stats aggregate complete.cases lm pf sd
+#'@importFrom utils write.csv
+#'@importFrom readxl read_excel
+
+
+
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+#if(getRversion() >= "2.15.1")  { utils::globalVariables(c("sd", "lm", "pf", "png", "par", )
+#}
+
--- R/extractXLSX_modis.R
+++ R/extractXLSX_modis.R
@@ -11,14 +11,14 @@
 
 extractXLSX_modis = function(dir = NULL, column = "MAX" ){
 
-  files = list.files(dir, full.names = T)
+  files = list.files(dir, full.names = T, pattern = ".xlsx")
   names = substr(list.files(dir), 1, 6)
 
   vals = col_lc
 
   for(i in seq_along(files)){
 
-    d1 = xlsx::read.xlsx(files[i], sheetIndex = 1)
+    d1 = read_excel(files[i])
     max = data.frame(d1[,"VALUE"], d1[,column]*.1)
     colnames(max) = c("code", names[i])
     vals = merge(vals, max, by= 'code', all.x = T)
